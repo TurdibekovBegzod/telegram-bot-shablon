d4c5fd8043137750a8a7abfa8a9369a6c31d392b
Begzod <begzodasidev@gmail.com>
2026-01-29 21:28:54 +0500

    config fayldan .env fayliga o'tildi

--- config.py
+++
@@ -1,6 +0,0 @@
-
-BOT_TOKEN = "YOUR BOT TOKEN"
-
-SUPERADMIN = "YOUR TELEGRAM ID"
-
-ADMINS = [] # list of the admins' ids
--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 from aiogram import Bot, Dispatcher
 from asyncio import run
-from config import BOT_TOKEN, SUPERADMIN
 from aiogram.types import BotCommand
 import admin
 import user
 import logging
 import sys
 import zoneinfo
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # 🔥 .env ni yuklaydi
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+SUPERADMIN = int(os.getenv("SUPERADMIN"))  # 🔥 string → int
 
 log_format = "%(asctime)s - %(levelname)s - %(message)s"
 date_format = "%Y-%m-%d %H:%M:%S"
 
-# logger configuration
 logging.basicConfig(
     level=logging.INFO,
     format=log_format,
@@ -21,23 +26,16 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 tz = zoneinfo.ZoneInfo("Asia/Tashkent")
 
 
+async def startup(bot: Bot):
+    await bot.send_message(text="Bot ishga tushdi! ✅", chat_id=SUPERADMIN)
 
-
-# -----------Umumiy ishlar------------------
-async def startup(bot : Bot):
-    await bot.send_message(text = "Bot ishga tushdi! ✅", chat_id=SUPERADMIN)
-
-async def shutdown(bot : Bot):
-    await bot.send_message(text = "Bot ishdan to'xtadi! ❌", chat_id=SUPERADMIN)
-
-
-
+async def shutdown(bot: Bot):
+    await bot.send_message(text="Bot ishdan to'xtadi! ❌", chat_id=SUPERADMIN)
 
 async def start():
     dp.startup.register(startup)
@@ -45,8 +43,6 @@ async def start():
 
     dp.include_router(admin.router)
     dp.include_router(user.router)
-
-    
 
     await bot.set_my_commands([
         BotCommand(command="start", description="Refresh the Bot!"),
